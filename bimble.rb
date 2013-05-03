9089a8648fcca480d2fce8f1c6d6040133be16e5
James Smith <james@floppy.org.uk>
2013-05-03 11:24:09 +0100

    refactor to clean up code

--- bimble.rb
+++ bimble.rb
@@ -7,43 +7,46 @@ require 'github_api'
 
 Dotenv.load
 
-Dir.mktmpdir do |tmpdir|
-
-  repo = Git.clone(ARGV[0], tmpdir)
-
-  Dir.chdir(tmpdir) do
-  
-    if File.exists?("Gemfile")
-  
-      `bundle update`
-  
-      branch = "update-dependencies-#{Date.today.to_s}"
-  
-      if repo.status.changed.keys.include? "Gemfile.lock"
-        branch = "update-dependencies-#{Date.today.to_s}"
-        repo.branch(branch).create
-        repo.checkout(branch)
-        repo.add("Gemfile.lock")
-        repo.commit("automatically updated dependencies")  
-        repo.push("origin", branch)
-      end
-    
-      if ENV['GITHUB_OAUTH_TOKEN']
-        github = Github.new oauth_token: ENV['GITHUB_OAUTH_TOKEN']
-
-        matches = repo.remote("origin").url.match(/\A.+:(.+?)\/([^\.]+).*\Z/)
-        if matches
-          user, repo = matches[1], matches[2]
-          github.pull_requests.create user, repo,
-                                      title: "automatically updated dependencies",
-                                      head: "#{user}:#{branch}",
-                                      base: "master"
-        end
-      
-      end
-    
-    end
-  
+def do_update(repo)
+  if File.exists?("Gemfile")
+    `bundle update`
+    branch = commit_to_new_branch(repo)
+    open_pull_request(repo, branch) if branch
   end
-  
+end
+
+def commit_to_new_branch(repo)
+  if repo.status.changed.keys.include? "Gemfile.lock"
+    branch = "update-dependencies-#{Date.today.to_s}"
+    repo.branch(branch).create
+    repo.checkout(branch)
+    repo.add("Gemfile.lock")
+    repo.commit("automatically updated dependencies")  
+    repo.push("origin", branch)
+    branch
+  else
+    nil
+  end
+end
+
+def open_pull_request(repo, branch)
+  if ENV['GITHUB_OAUTH_TOKEN']
+    github = Github.new oauth_token: ENV['GITHUB_OAUTH_TOKEN']
+
+    matches = repo.remote("origin").url.match(/\A.+:(.+?)\/([^\.]+).*\Z/)
+    if matches
+      user, repo_name = matches[1], matches[2]
+      github.pull_requests.create user, repo_name,
+                                  title: "automatically updated dependencies",
+                                  head: "#{user}:#{branch}",
+                                  base: "master"
+    end
+  end
+end
+
+Dir.mktmpdir do |tmpdir|
+  repo = Git.clone(ARGV[0], tmpdir)
+  Dir.chdir(tmpdir) do
+    do_update(repo)
+  end
 end
