7037d4585f1edf3915bd81ae93d10d3ca1e79444
James Smith <james@floppy.org.uk>
2014-03-11 11:17:10 +0000

    Move code into lib

--- bin/bimble.rb
+++ bin/bimble.rb
@@ -1,52 +1,9 @@
 #!/usr/bin/env ruby
 
-require 'git'
-require 'date'
 require 'dotenv'
-require 'github_api'
+require 'bimble'
 
 Dotenv.load
 
-def do_update(repo)
-  if File.exists?("Gemfile")
-    `bundle update`
-    branch = commit_to_new_branch(repo)
-    open_pull_request(repo, branch) if branch
-  end
-end
-
-def commit_to_new_branch(repo)
-  if repo.status.changed.keys.include? "Gemfile.lock"
-    branch = "update-dependencies-#{Date.today.to_s}"
-    repo.branch(branch).create
-    repo.checkout(branch)
-    repo.add("Gemfile.lock")
-    repo.commit("automatically updated dependencies")  
-    repo.push("origin", branch)
-    branch
-  else
-    nil
-  end
-end
-
-def open_pull_request(repo, branch)
-  if ENV['GITHUB_OAUTH_TOKEN']
-    github = Github.new oauth_token: ENV['GITHUB_OAUTH_TOKEN']
-
-    matches = repo.remote("origin").url.match(/\A.+:(.+?)\/([^\.]+).*\Z/)
-    if matches
-      user, repo_name = matches[1], matches[2]
-      github.pull_requests.create user, repo_name,
-                                  title: "automatically updated dependencies",
-                                  head: "#{user}:#{branch}",
-                                  base: "master"
-    end
-  end
-end
-
-Dir.mktmpdir do |tmpdir|
-  repo = Git.clone(ARGV[0], tmpdir)
-  Dir.chdir(tmpdir) do
-    do_update(repo)
-  end
-end
+bimble = Bimble::Runner.new(ENV['GITHUB_OAUTH_TOKEN'])
+bimble.update(ARGV[0])
--- lib/bimble.rb
+++ lib/bimble.rb
@@ -1,4 +1,5 @@
 require "bimble/version"
+require "bimble/runner"
 
 module Bimble
   # Your code goes here...
