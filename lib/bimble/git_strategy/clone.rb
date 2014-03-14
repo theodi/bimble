b1f46c7067956a1f109dea99e541c8d42e069fcf
James Smith <james@floppy.org.uk>
2014-03-14 14:47:22 +0000

    Add test for git strategy
    to check uniform interface

--- lib/bimble/git_strategy/clone.rb
+++ lib/bimble/git_strategy/clone.rb
@@ -11,24 +11,36 @@ class Bimble::GitStrategy::Clone
   end
 
   def update
+    in_working_copy do
+      Bimble.update
+      if lockfile_changed?
+        branch = commit_to_new_branch
+        open_pr(branch, "master") if @oauth_token
+      end
+    end
+  end
+
+  def in_working_copy
     Dir.mktmpdir do |tmpdir|
-      repository = Git.clone(@git_url, tmpdir)
-      Bimble.update(tmpdir)
-      if repository.status.changed.keys.include? "Gemfile.lock"
-        branch = commit_to_new_branch(repository)
-        open_pr(branch, "master")
+      @repository = Git.clone(@git_url, tmpdir)
+      Dir.chdir(tmpdir) do
+        yield
       end
     end
   end
 
   private
 
-  def commit_to_new_branch(repository)
-    repository.branch(branch_name).create
-    repository.checkout(branch_name)
-    repository.add("Gemfile.lock")
-    repository.commit(commit_message)  
-    repository.push("origin", branch_name)
+  def lockfile_changed?
+    @repository.status.changed.keys.include? "Gemfile.lock"
+  end
+
+  def commit_to_new_branch
+    @repository.branch(branch_name).create
+    @repository.checkout(branch_name)
+    @repository.add("Gemfile.lock")
+    @repository.commit(commit_message)  
+    @repository.push("origin", branch_name)
     branch_name
   end
 
---
+++ spec/git_strategy_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+[
+  :clone,
+  :github_api,
+].each do |strategy|
+
+  describe "#{strategy} strategy", :vcr do
+    
+    before :all do
+      @bimble = Bimble.create(strategy, 
+                              git_url: 'git@github.com:Floppy/bimble-test.git', 
+                              github_oauth_token: ENV['GITHUB_OAUTH_TOKEN'])
+    end
+    
+    it "should get all appropriate files" do
+      @bimble.in_working_copy do
+        File.exists?('Gemfile').should be_true
+        File.exists?('Gemfile.lock').should be_true
+        File.exists?('bimble-test.gemspec').should be_true
+      end
+    end
+  
+  end
+  
+end
