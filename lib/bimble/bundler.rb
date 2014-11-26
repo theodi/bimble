f7b54438dac00dd119b99490a353273fdd40f203
James Smith <james@floppy.org.uk>
2014-11-26 17:08:35 +0000

    ignore local bundler config

--- lib/bimble/bundler.rb
+++ lib/bimble/bundler.rb
@@ -3,7 +3,7 @@ module Bimble
   def self.bundle_update
     if File.exists?("Gemfile")
       Bundler.with_clean_env do
-        `sh -c "bundle update"`
+        puts `sh -c "BUNDLE_IGNORE_CONFIG=true bundle update"`
       end
     end
   end
